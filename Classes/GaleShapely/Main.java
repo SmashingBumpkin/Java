a86140cce40c472a0701a619484eb94dd4cf7b81
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-02-26 15:53:27 +0100

    CA and algos
    ca

---
+++ Classes/GaleShapely/Main.java
@@ -0,0 +1,14 @@
+package Classes.GaleShapely;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hiii");
+    }
+
+    public static String[] preference_generator(int n) {
+        String[] output = new String[n];
+        
+
+        return output;
+    }
+}
--- RISCV/riscv1.asm
+++ RISCV/riscv1.asm
@@ -1,8 +1,17 @@
-addi t1, zero, 1
-add t2, t1, t1
-add t2, t2, t2
-add t2, t2, t2
-add t2, t2, t2
-sw t2, -100
-lw t3, -100
-lw t4, -100
+#Instructions add, sub, or, addi, ori, lui, lw, and sw. Rars Risc-V Assembler and Runtime Simulator.
+
+#Exercise 1: Write the code to load from memory two integers, sum them up, and store the result into the memory.
+.data # By default starts at address 0x10010000
+
+    .word 17
+    .word 25
+   
+.text # By default starts at address 0x00400000
+
+    lui x5, 0x10010
+    lw x3, 0(x5)
+    lw x4, 4(x5)
+    add x6, x3, x4
+    sw x6, 8(x5)
+    ori x17, x0, 10
+    ecall
