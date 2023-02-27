40765154b1838e441372520ac6d72a03fbfc5a95
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-02-27 17:04:15 +0100

    ...

--- Classes/GaleShapely/Main.java
+++ Classes/GaleShapely/Main.java
@@ -1,12 +1,20 @@
 package Classes.GaleShapely;
 
 public class Main {
+    String[] a1 = new String[] {"b1", "b2", "b3"};
+    String[] a2 = new String[] {"b1", "b3", "b2"};
+    String[] a3 = new String[] {"b3", "b1", "b2"};
+    String[] b1 = new String[] {"a2", "b1", "b3"};
+    String[] b2 = new String[] {"a1", "a3", "a2"};
+    String[] b3 = new String[] {"a2", "a1", "a3"};
     public static void main(String[] args) {
         System.out.println("Hiii");
     }
 
     public static String[] preference_generator(int n) {
         String[] output = new String[n];
+
+
         
 
         return output;
