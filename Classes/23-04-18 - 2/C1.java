ab78d26659ff83d8e8f5365d79a2bde55c0525f2
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-04-18 12:04:28 +0200

    april 18th class stuff

---
+++ Classes/23-04-18 - 2/C1.java
@@ -0,0 +1,6 @@
+/**
+ * Created by pietrocenciarelli on 31/03/21.
+ */
+
+public class C1 {
+}
---
+++ Classes/23-04-18 - 2/Gen.java
@@ -0,0 +1,13 @@
+/**
+ * Created by pietrocenciarelli on 31/03/21.
+ */
+public class Gen <E>  {
+
+    public void printGnat () {
+        System.out.println("gnat");
+    }
+
+    public void printParam (E x) {
+        System.out.println(x);
+    }
+}
