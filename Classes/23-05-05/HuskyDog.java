b484e96ef4f034849d67072f73e8faace4d24ddd
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-05-09 09:26:31 +0200

    lesson 05/05/23

---
+++ Classes/23-05-05/HuskyDog.java
@@ -0,0 +1,22 @@
+/**
+ * Created by pietrocenciarelli on 11/05/22.
+ */
+public class HuskyDog {
+
+    public String name;
+
+    public HuskyDog () {
+        this.name = "Lokino";
+    }
+
+    public HuskyDog (String name) {
+        this.name = name;
+    }
+
+    public void eatChicken () {
+        System.out.println(name + ": chicken, I eat you!");
+    }
+    public void runAway () {
+        System.out.println(name + ": runs like the wind!");
+    }
+}
